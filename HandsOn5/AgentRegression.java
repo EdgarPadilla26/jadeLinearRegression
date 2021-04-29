847184d789cfad0b4c74e324bac92f85d2b793b3
EdgarPadilla26 <71788841+EdgarPadilla26@users.noreply.github.com>
2021-04-29 13:03:15 -0500

    Add files via upload

---
+++ HandsOn5/AgentRegression.java
@@ -0,0 +1,36 @@
+package examples.handson;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+
+
+public class AgentRegression extends Agent {
+
+    protected void setup() {
+
+        System.out.println("\n This is the Simple Linear Regression Formula\n");
+        System.out.println("y = Beta0 + Beta1x \n");
+        //OneShotBehaviour
+        addBehaviour(new Calcular());
+    }
+
+    public class Calcular extends OneShotBehaviour {
+        
+        public void action() {
+
+            int adv[] = {23,26,30,34,43,48,52,57,58};
+            int sales[] = {651,762,856,1063,1190,1298,1421,1440,1518};
+
+            LinearRegression obj = new LinearRegression(adv,sales);
+            obj.regression();
+
+            System.out.println("\n This is the Linear Regression Formula with dataSet \n");
+            System.out.println("\nSales= " + obj.B0 + " + " + obj.B1 +"Advertising\n");
+
+            System.out.println("\nIf Benneton invests 80ME in advertisings this is the result\n");
+            float Sales = (obj.B0+obj.B1*80);
+            System.out.println("Sales= " +  Sales);
+        }  
+    }   
+}
